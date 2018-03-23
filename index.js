27f3ae0cc83d821d0b924e3a98ec16e313d0d9b8
Alena Khineika <>
2018-03-23 12:15:42 +0100

    Python generator

--- index.js
+++ index.js
@@ -1,72 +1,26 @@
-const path = require('path');
-const Transformer = require('./transformer');
+const antlr4 = require('antlr4');
+const ECMAScriptLexer = require('./lib/ECMAScriptLexer.js');
+const ECMAScriptParser = require('./lib/ECMAScriptParser.js');
 
-const ECMAScriptLexer = require(path.resolve('lib', 'ECMAScriptLexer'));
-const ECMAScriptParser = require(path.resolve('lib', 'ECMAScriptParser'));
-const ECMAScriptListener = require(path.resolve('lib', 'ECMAScriptListener'));
-const ECMAScriptVisitor = require(path.resolve('lib', 'ECMAScriptVisitor'));
+const PythonGenerator = require('./codegeneration/PythonGenerator.js');
 
-const CSharpLexer = require(path.resolve('lib', 'CSharpLexer'));
-const CSharpParser = require(path.resolve('lib', 'CSharpParser'));
+const input = '{x: new Number(1)}';
 
-const jsInput = `{ $text: { $search: "java coffee shop" } }`;
+const chars = new antlr4.InputStream(input);
+const lexer = new ECMAScriptLexer.ECMAScriptLexer(chars);
 
-console.log(jsInput);
+lexer.strictMode = false; // do not use js strictMode
 
-/*
-const jsInput = `{
-	var x = 4;
-	
-	db.bios.findOne({$test: 1});
-	db.bios.findTwo([1, 2, 3]);
-}`;
-*/
+const tokens = new antlr4.CommonTokenStream(lexer);
+const parser = new ECMAScriptParser.ECMAScriptParser(tokens);
+const tree = parser.expressionSequence();
 
-const jsTransformer = new Transformer(jsInput, {
-	lexer: ECMAScriptLexer.ECMAScriptLexer,
-	parser: ECMAScriptParser.ECMAScriptParser,
-	listener: ECMAScriptListener.ECMAScriptListener,
-	visitor: ECMAScriptVisitor.ECMAScriptVisitor
-});
+console.log('JavaScript input:');
+console.log(input);
 
-const jsTree = jsTransformer.getTree();
-const jsAST = jsTransformer.getAST();
+const output = new PythonGenerator().start(tree);
 
-// console.log('///////// JS TREE /////////\n');
-
-jsTransformer.walk();
-const output = jsTransformer.getListenerOutput()
-
-// jsTransformer.visit();
-// const output = jsTransformer.getVisitorOutput()
-
+console.log('Python output:');
 console.log(output);
 
-/*
-console.log('\n');
-console.log(jsTree);
-console.log('\nOR\n');
-console.log(jsAST);
-*/
-
-/*
-const csInput = `
-	public class Car {
-		int speedIncrease = 10;
-	}
-`;
-
-const csTransformer = new Transformer(csInput, {
-	lexer: CSharpLexer.CSharpLexer,
-	parser: CSharpParser.CSharpParser
-});
-
-const csTree = csTransformer.getTree();
-const csAST = csTransformer.getAST();
-
-console.log('\n');
-console.log('///////// CSHARP TREE /////////\n');
-console.log(csTree);
-console.log('\nOR\n');
-console.log(csAST);
-*/
+// console.log(tree.toStringTree(parser.ruleNames));
